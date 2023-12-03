41185683c89e3818c0809163a05ba439edf4d1bc
Najib Aseri <najibaseri@gmail.com>
2023-12-03 15:44:41 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,33 +1,53 @@
 // Create web server
-// 1. Start the server
-// 2. Handle request and response
-// 3. Return the result
+// 1. npm init -y
+// 2. npm i express
+// 3. node comment.js
+// 4. http://localhost:3000
+// 5. http://localhost:3000/api/comments
 
-// 1. Start the server
-// 1.1 Import http module
-var http = require('http');
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const bodyParser = require('body-parser')
 
-// 1.2 Create server
-var server = http.createServer();
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
-// 1.3 Start server, and listen to port 3000
-server.listen(3000, function() {
-    console.log('Server is running on port 3000');
-});
+app.use(express.static(path.join(__dirname, 'public')))
 
-// 2. Handle request and response
-// 2.1 Event: request
-// This event is fired when a client send request
-// to current server
-server.on('request', function(request, response) {
-    console.log('Server received a request');
-    console.log('Method: ', request.method);
-    console.log('URL: ', request.url);
-    console.log('Headers: ', request.headers);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
-    // 2.2 Event: response
-    // This event is fired when server send response
-    // to client
-    response.write('Hello');
-    response.end();
-});
+app.get('/api/comments', (req, res) => {
+    fs.readFile(path.join(__dirname, 'data', 'comments.json'), (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+            return
+        }
+        res.send(JSON.parse(data))
+    })
+})
+
+app.post('/api/comments', (req, res) => {
+    fs.readFile(path.join(__dirname, 'data', 'comments.json'), (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+            return
+        }
+        const comments = JSON.parse(data)
+        comments.push(req.body)
+        fs.writeFile(path.join(__dirname, 'data', 'comments.json'), JSON.stringify(comments), (err) => {
+            if (err) {
+                res.status(500).send(err)
+                return
+            }
+            res.send(comments)
+        })
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server start on port 3000')
+})
